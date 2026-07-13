6afe3daf32e0b115422483ec149535c77b70e98b
Snekha <snekha717@gmail.com>
2026-07-13 23:52:17 +0530

    Add files via upload

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export interface Post {
+  id: string; // MockAPI uses strings for IDs
+  title: string;
+  body: string;
+  thumbnail: string;
+  authorName: string;
+  authorAvatar: string;
+  createdAt: string;
+}
